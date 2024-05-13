1b29c60730f5e93cc13725ce61dcd72c71dee477
Rushin Shah <shahrushin32@gmail.com>
2024-05-14 00:58:01 +0530

    added aggregation features to API.

--- Controllers/toursController.js
+++ Controllers/toursController.js
@@ -93,3 +93,93 @@ exports.deleteTour = async (req, res) => {
 		})
 	}
 };
+
+exports.tourStats = async (req, res) => {
+	try{
+		const stats = await Tour.aggregate([
+			{ 
+				$match: {ratingsAverage: { $gte : 4.5}}
+			},
+			{
+				$group: {
+					_id: { $toUpper: '$difficulty'},
+					toursNum: { $sum: 1},
+					avgRating: { $avg: '$ratingsAverage'},
+					avgPrice: { $avg: '$price'},
+					minPrice: { $min : '$price'},
+					maxPrice: {$max : '$price'}
+
+				}
+			},
+			{
+				$sort: { minPrice: -1}
+			}
+		])
+
+		res.status(200).json({
+			status: "success",
+			data:{
+				stats
+			}
+		})
+	}catch(err){
+		res.status(400).json({
+			status: "fail",
+			err
+		})
+	}
+}
+
+exports.monthlyPlan = async (req, res) => {
+	try{
+		const year = req.params.year * 1;
+		const plan = await Tour.aggregate([
+			{
+				$unwind: '$startDates'
+			},
+			{
+				$match: { 
+					startDates : { 
+						$gte: new Date(`${year}-01-01`), $lte : new Date(`${year}-12-31`)
+					}
+				}
+			},
+			{
+				$group: {
+					_id: { $month : '$startDates'},
+					numsOfTours: { $sum : 1},
+					tours: { $push: '$name'},
+				}
+			},
+			{
+				$addFields : {
+					month: '$_id'
+				}
+			},
+			{
+				$project: {
+					_id: 0
+				}
+			},
+			{
+				$sort: {
+					numsOfTours: -1
+				}
+			}
+		]);
+
+		res.status(200).json({
+			status: "success",
+			data:{
+				plan
+			}
+
+		})
+
+	} catch(err){
+		res.status(400).json({
+			status: "fail",
+			err
+		})
+	}
+}
--- Routers/toursRouter.js
+++ Routers/toursRouter.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 // router.param("id", toursController.checkID);
 router.route("/").get(toursController.getAllTours).post(toursController.createTour);
+router.route("/tour-stats").get(toursController.tourStats);
+router.route("/monthly-plan/:year").get(toursController.monthlyPlan);
 
 router.route("/:id").get(toursController.getATour).patch(toursController.patchTour).delete(toursController.deleteTour);
 
