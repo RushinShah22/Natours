88696971350aa8f236d8a5085455fc66e1b088c0
Rushin Shah <shahrushin32@gmail.com>
2024-05-14 11:48:59 +0530

    added better validation for tour model.

--- Model/tourModel.js
+++ Model/tourModel.js
@@ -4,7 +4,8 @@ const tourSchema = new mongoose.Schema({
 	name: {
 		required: [true, "A tour must have a name."],
 		type: String,
-		unique: true
+		unique: true,
+		trim: true
 	},
 	duration:{
 		type: Number,
@@ -16,7 +17,11 @@ const tourSchema = new mongoose.Schema({
 	},
 	difficulty:{
 		type: String,
-		required: [true, "a tour must have a difficulty."]
+		required: [true, "a tour must have a difficulty."],
+		enum : {
+			values: ['easy', 'medium', 'difficult'],
+			message: "difficulty should be either: easy, medium, or difficult."
+		}
 	},
 	ratingsQuantity:{
 		type: Number,
@@ -24,22 +29,34 @@ const tourSchema = new mongoose.Schema({
 	},
 	ratingsAverage:{
 		default: 4.5,
-		type: Number
+		type: Number,
+		min: [1, "rating should be atleast 1.0"],
+		max: [5, "rating can be atmost 5.0"]
 	},
 
 	price: {
 		required: [true, "A tour must have a price."],
 		type: Number
 	},
-	priceDiscount: Number,
+	priceDiscount: {
+		type: Number,
+		validate: {
+			validator: function(val){
+				return this.price >= val;
+			},
+			message: "Discount ({VALUE}) can be atmost equal to price."
+		}
+	},
 	summary:{
 		type: String,
 		trim: true,
-		required: [true, "A tour must have a summary."]
+		required: [true, "A tour must have a summary."],
+		maxLength: [200, "Summary can be of atmost 200 characters."]
 	},
 	description:{
 		type: String,
-		trim: true
+		trim: true,
+		maxLength: [1000, "Description can be of atmost 500 characters."]
 	},
 	imageCover:{
 		type: String,
