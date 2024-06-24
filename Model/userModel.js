9fff4459d6598c942b3e107afe3acd35f09040c6
Rushin Shah <shahrushin32@gmail.com>
2024-06-24 12:39:58 +0530

    implemented user model.

---
+++ Model/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is mandatory"],
+        trim: true,
+        maxlength: [50, "A user name can be atmost 50 characters long."]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is mandatory."],
+        unique: true,
+        lowercase: true,
+        validate: {
+            validator: validator.isEmail,
+            message: "Please provide a valid email."
+        }
+    },
+    photo: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: [true, "Password is mandatory."],
+        minlength: [8, "Password must be atleast 8 characters long."]
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, "Confirming Password is mandatory."]
+    }
+})
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
