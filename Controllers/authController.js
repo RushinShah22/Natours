766e8b7dde06a1322a8298fbdf5b27ef3cbb9efb
Rushin Shah <shahrushin32@gmail.com>
2024-08-09 00:39:21 +0530

    implement jwt creation.

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,12 +1,21 @@
 const UserModel = require('./../Model/userModel');
 const catchAsyncError = require('./../utils/catchAsyncError');
+const jwt = require('jsonwebtoken');
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '90d',
+  });
+};
 
 exports.signup = catchAsyncError(async (req, res) => {
   const newUser = await UserModel.create(req.body);
+  const token = createToken(newUser._id);
   res.status(201).json({
     status: 'success',
     data: {
       user: newUser,
+      token,
     },
   });
 });
