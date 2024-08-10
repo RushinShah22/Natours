3d8a29a9ba843d879691df49e5e50d14c40f3a7e
Rushin Shah <shahrushin32@gmail.com>
2024-08-10 15:44:03 +0530

    add secure middleware.

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
 const express = require('express');
-const morgan = require("morgan");
-const tourRouter = require("./Routers/toursRouter");
-const AppError = require("./utils/appError")
-const globalErrorHandler = require("./Controllers/errorController")
-const userRouter = require("./Routers/userRouter")
-
+const morgan = require('morgan');
+const tourRouter = require('./Routers/toursRouter');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./Controllers/errorController');
+const userRouter = require('./Routers/userRouter');
+const xss = require('xss-clean');
+const expressMongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
 
 const app = express();
-app.use(express.json());
 
-if(process.env.NODE_ENV === "development"){
-	app.use(morgan('dev'));
+app.use(
+  rateLimit({
+    max: 100,
+    windowMS: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!',
+  })
+);
+
+app.use(expressMongoSanitize());
+app.use(helmet());
+app.use(express.json({ limit: '10kb' }));
+
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: ['difficulty', 'duration', 'ratingAverage', 'ratings'],
+  })
+);
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-app.all("*", (req, res, next) => {
-	next((new AppError(`can't find ${req.originalUrl} on this server.`, 404, "fail")));
-})
+app.all('*', (req, res, next) => {
+  next(
+    new AppError(`can't find ${req.originalUrl} on this server.`, 404, 'fail')
+  );
+});
 
 app.use(globalErrorHandler);
 
-
-
 module.exports = app;
-
