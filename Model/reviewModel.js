f16f7a742c0eb6d7e2b83465d216c7bc55e6390a
Rushin Shah <shahrushin32@gmail.com>
2024-09-23 00:11:45 +0530

    implement review model.

---
+++ Model/reviewModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      minLength: [10, 'a review must be atleast 10 characters long.'],
+      required: [true, 'a review needs a review.'],
+    },
+    rating: {
+      type: Number,
+      required: [true, 'a review must have a rating.'],
+      min: 0,
+      max: 5,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'a review must belong to a user.'],
+    },
+
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'a review must belong to a tour.'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.Model('Review', reviewSchema);
+
+modules.exports = Review;
--- server.dockerfile
+++ server.dockerfile
@@ -2,11 +2,16 @@ FROM node
 
 RUN npm install --global nodemon
 WORKDIR /usr/natours
+
 ENV NODE_ENV=production
 ENV PORT=80
 EXPOSE 80
+
+COPY ./package.json ./
+COPY ./package-lock.json ./
+RUN npm install
+
 COPY . .
 
-RUN npm install
 
 CMD ["npm", "start"]
